78d74d8c76bca5dcbc341605d793c9faede53801
Pmc Dev <phoomparin@gmail.com>
2016-12-07 10:38:02 +0700

    Implemented schema-oriented conversation format

--- src/components/ChatInterface/ChatInterface.js
+++ src/components/ChatInterface/ChatInterface.js
@@ -4,11 +4,22 @@ import c from "classnames"
 
 import s from "./ChatInterface.scss"
 
+const _ChatContent = ({src}) => {
+  if (typeof src === "string") {
+    return <span dangerouslySetInnerHTML={{__html: src}} />
+  } else if (src.image) {
+    return <img src={src.image} alt="Something" />
+  }
+  return null
+}
+
+const ChatContent = withStyles(s)(_ChatContent)
+
 const ChatInterface = props => (
   <div className={c(s.chat, s.chatAvatarEnabled)}>
     <ol className={s.chatList}>
       {
-        props.backlog.map((chat, index) => {
+        props.backlog ? props.backlog.map((chat, index) => {
           const user = props.user[chat.user]
           const client = user.client // Is user
           const iCF = (!client && chat.showAvatar)
@@ -21,32 +32,32 @@ const ChatInterface = props => (
                 </div>
               )}
               <div
-                onClick={() => props.setTyping(index)}
                 className={c(
                   s.chatBubble,
                   client && s.chatBubbleResponse,
                   (props.typing[index] && !client) && s.chatBubbleTyping,
+                  chat.text.image && s.chatCard,
                   s.chatBubbleFade,
                   s.chatBubbleSlideIn
                 )}
               >
-                <span dangerouslySetInnerHTML={{__html: `${chat.text}`}} />
+                <ChatContent src={chat.text} />
               </div>
             </li>
           )
-        })
+        }) : null
       }
     </ol>
     <div className={s.chatResponses}>
       {
-        props.choices.map((choice, i) => (
+        props.choices ? props.choices.map((choice, i) => (
           <div
             key={i}
             className={c(s.chatBubble, s.chatBubbleResponse)}
-            dangerouslySetInnerHTML={{__html: choice}}
+            dangerouslySetInnerHTML={{__html: choice.text}}
             onClick={() => props.onChoiceSelected(i)}
           />
-        ))
+        )) : null
       }
     </div>
   </div>
